10866bfbdeb5b261215c31d9bdb36957a0df8e22
Heart1ess1 <166058529+Heart1ess1@users.noreply.github.com>
2024-10-10 16:51:23 +0800

    Update 4_vpd_platform_admin_trigger.sql

--- 4_vpd_platform_admin_trigger.sql
+++ 4_vpd_platform_admin_trigger.sql
@@ -1,29 +1,38 @@
+-- Create a context namespace called PLATFORMADMIN_CTX using the package PLATFORMADMIN_CTX_PKG
 CREATE OR REPLACE CONTEXT PLATFORMADMIN_CTX USING PLATFORMADMIN_CTX_PKG;
 
+-- Create the package specification for PLATFORMADMIN_CTX_PKG
 CREATE OR REPLACE PACKAGE PLATFORMADMIN_CTX_PKG IS
+    -- Declare a procedure to set the platform administrator context
     PROCEDURE SET_PLATFORMADMIN_CONTEXT;
 END;
 /
 
+-- Create the package body for PLATFORMADMIN_CTX_PKG
 CREATE OR REPLACE PACKAGE BODY PLATFORMADMIN_CTX_PKG IS
+    -- Define the procedure to set the platform administrator context
     PROCEDURE SET_PLATFORMADMIN_CONTEXT IS
         ADMIN_ID NUMBER;
     BEGIN
+        -- Retrieve the AdminID based on the current session user
         SELECT AdminID INTO ADMIN_ID
         FROM PlatformAdmins
         WHERE Username = SYS_CONTEXT('USERENV', 'SESSION_USER');
 
+        -- Set the admin_id in the platformadmin_ctx context
         DBMS_SESSION.SET_CONTEXT('platformadmin_ctx', 'admin_id', ADMIN_ID);
     EXCEPTION
         WHEN NO_DATA_FOUND THEN
-            NULL; 
+            NULL; -- If no matching record is found, do nothing
     END SET_PLATFORMADMIN_CONTEXT;
 END;
 /
 
+-- Create a trigger that fires after a user logs on to the database
 CREATE OR REPLACE TRIGGER SET_PLATFORMADMIN_CTX_TRIG
 AFTER LOGON ON DATABASE
 BEGIN
+    -- Call the procedure to set the platform administrator context
     PLATFORMADMIN_CTX_PKG.SET_PLATFORMADMIN_CONTEXT;
 END;
 /
