58a5e407e7245f6a44c09d2ebe106d0862812f26
Heart1ess1 <166058529+Heart1ess1@users.noreply.github.com>
2024-10-15 20:56:07 +0800

    Update 4_vpd_store_staff_trigger.sql

--- 4_vpd_store_staff_trigger.sql
+++ 4_vpd_store_staff_trigger.sql
@@ -2,15 +2,16 @@
 CREATE OR REPLACE CONTEXT STORESTAFF_CTX USING STORESTAFF_CTX_PKG;
 
 -- Create the package specification for STORESTAFF_CTX_PKG
-CREATE OR REPLACE PACKAGE STORESTAFF_CTX_PKG IS
+CREATE OR REPLACE PACKAGE STORESTAFF_CTX_PKG AUTHID CURRENT_USER IS
     -- Declare a procedure to set the store staff context
     PROCEDURE SET_STORESTAFF_CONTEXT;
 END;
 /
 
 -- Create the package body for STORESTAFF_CTX_PKG
+-- Create PACKAGE BODY
 CREATE OR REPLACE PACKAGE BODY STORESTAFF_CTX_PKG IS
-    -- Define the procedure to set the store staff context
+    -- Define a procedure to set the store staff context
     PROCEDURE SET_STORESTAFF_CONTEXT IS
         STAFF_ID NUMBER;
         STORE_ID NUMBER;
@@ -19,9 +20,9 @@ CREATE OR REPLACE PACKAGE BODY STORESTAFF_CTX_PKG IS
         -- Retrieve StaffID, StoreID, and Role based on the current session user
         SELECT StaffID, StoreID, Role INTO STAFF_ID, STORE_ID, ROLE
         FROM StoreStaff
-        WHERE Username = SYS_CONTEXT('USERENV', 'SESSION_USER');
+        WHERE UPPER(Username) = UPPER(SYS_CONTEXT('USERENV', 'SESSION_USER'));
 
-        -- Set the retrieved values into the storestaff_ctx context
+        -- Set the retrieved values in the storestaff_ctx context
         DBMS_SESSION.SET_CONTEXT('storestaff_ctx', 'staff_id', STAFF_ID);
         DBMS_SESSION.SET_CONTEXT('storestaff_ctx', 'store_id', STORE_ID);
         DBMS_SESSION.SET_CONTEXT('storestaff_ctx', 'role', ROLE);
@@ -31,6 +32,7 @@ CREATE OR REPLACE PACKAGE BODY STORESTAFF_CTX_PKG IS
     END SET_STORESTAFF_CONTEXT;
 END;
 /
+
 
 -- Create a trigger that fires after a user logs on to the database
 CREATE OR REPLACE TRIGGER SET_STORESTAFF_CTX_TRIG
