c021de6077e7bddd08f57079aeaff29a27ede99f
Heart1ess1 <166058529+Heart1ess1@users.noreply.github.com>
2024-10-15 20:52:32 +0800

    Update 3_vpd_consumer_trigger.sql

--- 3_vpd_consumer_trigger.sql
+++ 3_vpd_consumer_trigger.sql
@@ -2,28 +2,24 @@
 CREATE OR REPLACE CONTEXT CUSTOMER_CTX USING CUSTOMER_CTX_PKG;
 
 -- Create a package specification for CUSTOMER_CTX_PKG
-CREATE OR REPLACE PACKAGE CUSTOMER_CTX_PKG IS
-    -- Declare a procedure to set the customer context
+CREATE OR REPLACE PACKAGE CUSTOMER_CTX_PKG AUTHID CURRENT_USER IS
     PROCEDURE SET_CUSTOMER_CONTEXT;
 END;
 /
 
 -- Create the package body for CUSTOMER_CTX_PKG
 CREATE OR REPLACE PACKAGE BODY CUSTOMER_CTX_PKG IS
-    -- Define the procedure to set the customer context
     PROCEDURE SET_CUSTOMER_CONTEXT IS
         CUSTOMER_ID NUMBER;
     BEGIN
-        -- Retrieve the CustomerID based on the current session user
         SELECT CustomerID INTO CUSTOMER_ID
         FROM Consumers
         WHERE Username = SYS_CONTEXT('USERENV', 'SESSION_USER');
 
-        -- Set the customer_id in the customer_ctx context
         DBMS_SESSION.SET_CONTEXT('customer_ctx', 'customer_id', CUSTOMER_ID);
     EXCEPTION
         WHEN NO_DATA_FOUND THEN
-            NULL; -- If no matching record is found, do nothing
+            NULL;
     END SET_CUSTOMER_CONTEXT;
 END;
 /
