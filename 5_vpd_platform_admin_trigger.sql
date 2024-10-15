53282d47a3d2a48cffcdf6b5ea9214a6cb607c6e
Heart1ess1 <166058529+Heart1ess1@users.noreply.github.com>
2024-10-15 21:02:28 +0800

    Update 5_vpd_platform_admin_trigger.sql

--- 5_vpd_platform_admin_trigger.sql
+++ 5_vpd_platform_admin_trigger.sql
@@ -2,24 +2,25 @@
 CREATE OR REPLACE CONTEXT PLATFORMADMIN_CTX USING PLATFORMADMIN_CTX_PKG;
 
 -- Create the package specification for PLATFORMADMIN_CTX_PKG
-CREATE OR REPLACE PACKAGE PLATFORMADMIN_CTX_PKG IS
+CREATE OR REPLACE PACKAGE PLATFORMADMIN_CTX_PKG AUTHID CURRENT_USER IS
     -- Declare a procedure to set the platform administrator context
     PROCEDURE SET_PLATFORMADMIN_CONTEXT;
 END;
 /
 
 -- Create the package body for PLATFORMADMIN_CTX_PKG
+-- Create PACKAGE BODY
 CREATE OR REPLACE PACKAGE BODY PLATFORMADMIN_CTX_PKG IS
-    -- Define the procedure to set the platform administrator context
+    -- Define a procedure to set the platform admin context
     PROCEDURE SET_PLATFORMADMIN_CONTEXT IS
         ADMIN_ID NUMBER;
     BEGIN
-        -- Retrieve the AdminID based on the current session user
+        -- Retrieve AdminID based on the current session user
         SELECT AdminID INTO ADMIN_ID
         FROM PlatformAdmins
-        WHERE Username = SYS_CONTEXT('USERENV', 'SESSION_USER');
+        WHERE UPPER(Username) = UPPER(SYS_CONTEXT('USERENV', 'SESSION_USER'));
 
-        -- Set the admin_id in the platformadmin_ctx context
+        -- Set admin_id in the platformadmin_ctx context
         DBMS_SESSION.SET_CONTEXT('platformadmin_ctx', 'admin_id', ADMIN_ID);
     EXCEPTION
         WHEN NO_DATA_FOUND THEN
@@ -27,6 +28,7 @@ CREATE OR REPLACE PACKAGE BODY PLATFORMADMIN_CTX_PKG IS
     END SET_PLATFORMADMIN_CONTEXT;
 END;
 /
+
 
 -- Create a trigger that fires after a user logs on to the database
 CREATE OR REPLACE TRIGGER SET_PLATFORMADMIN_CTX_TRIG
